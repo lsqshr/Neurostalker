e699e18318f69fbebdb8a70d9a5f2c61863cceb8
siqi liu <lsqshr@gmail.com>
2015-05-02 13:49:22 +1000

    add saveRTree; Further dev the rtree converting util

--- stalker_v3d/NeuroStalker.pro
+++ stalker_v3d/NeuroStalker.pro
@@ -2,21 +2,9 @@
 TEMPLATE = lib
 CONFIG	+= qt plugin warn_off
 #CONFIG	+= x86_64
-#CONFIG += use_static_libs
-VAA3DPATH = /home/siqi/workspace/v3d_external
+VAA3DPATH = ../../../v3d_external
 INCLUDEPATH	+= $$VAA3DPATH/v3d_main/basic_c_fun
 INCLUDEPATH	+= $$VAA3DPATH/v3d_main/common_lib/include
-
-#OpenCV headers and library folders
-INCLUDEPATH += /usr/local/include/opencv
-LIBS+= -L/usr/local/lib
-
-#LIBS+= -lopencv_core -lopencv_imgproc -lopencv_highgui \
-#		-lzlib \
-#		-llibtiff \
-#		-llibjpeg \
-#		-lIlmImf \
-#		-llibjasper
 
 HEADERS	+= NeuroStalker_plugin.h
 HEADERS	+= Stalker.h
@@ -24,7 +12,6 @@ SOURCES	+= NeuroStalker_plugin.cpp
 SOURCES	+= Stalker.cpp
 SOURCES	+= $$VAA3DPATH/v3d_main/basic_c_fun/v3d_message.cpp
 SOURCES	+= $$VAA3DPATH/v3d_main/basic_c_fun/basic_surf_objs.cpp
-
 
 TARGET	= $$qtLibraryTarget(NeuroStalker)
 DESTDIR	= $$VAA3DPATH/bin/plugins/bigneuronhackathon/NeuroStalker/
--- stalker_v3d/utils/mattree2opencv.m
+++ stalker_v3d/utils/mattree2opencv.m
@@ -3,8 +3,8 @@ function matrtree2opencv(matrtreepath)
 % compatible xml file
 % WIP
 
-matrtree = load(matrtreepath, 'model_alpha');
-matrtree = matrtree.model_alpha;
+matrtree = load(matrtreepath, 't');
+matrtree = matrtree.t;
 
 % --- Top layer of the xml
 docNode = com.mathworks.xml.XMLUtils.createDocument('opencv_storage');
@@ -18,34 +18,31 @@ formatNode.appendChild(docNode.createTextNode('3'));
 mlrtreesNode.appendChild(formatNode);
 
 isclassifierNode = docNode.createElement('is_classifier');
-if strcmp(matrtree.Method, 'regresssion')
-    isclassifierNode.appendChild(docNode.createTextNode('0'));
-else
-    isclassifierNode.appendChild(docNode.createTextNode('1'));
-end
+isclassifierNode.appendChild(docNode.createTextNode(num2str(matrtree.is_classifier)));
 mlrtreesNode.appendChild(isclassifierNode);
 
 % TODO: DH, pls help me check the meaning of these parameters
 % For the parameters, you may find this helpful 
 % http://docs.opencv.org/2.4.9/modules/ml/doc/decision_trees.html
 varallNode = docNode.createElement('var_all');
-varallNode.appendChild(docNode.createTextNode('0'));
+varallNode.appendChild(docNode.createTextNode(num2str(matrtree.var_count+1)));
 mlrtreesNode.appendChild(varallNode);
 
 varcountNode = docNode.createElement('var_count');
-varcountNode.appendChild(docNode.createTextNode('0'));
+varcountNode.appendChild(docNode.createTextNode(num2str(matrtree.var_count)));
 mlrtreesNode.appendChild(varcountNode);
 
 ordvarcountNode = docNode.createElement('ord_var_count'); % Seems for regression all var should be ordered
 % http://stackoverflow.com/questions/24327230/using-opencv-random-forest-for-regression
-ordvarcountNode.appendChild(docNode.createTextNode('0'));
+ordvarcountNode.appendChild(docNode.createTextNode(num2str(matrtree.var_count+1)));
 mlrtreesNode.appendChild(ordvarcountNode);
 
 catvarcountNode = docNode.createElement('cat_var_count');
-catvarcountNode.appendChild(docNode.createTextNode('0'));
+catvarcountNode.appendChild(docNode.createTextNode(num2str(0)));
 mlrtreesNode.appendChild(varcountNode);
 
-% --- Third layer training_params
+% --- Third layer training_params: Keep them as 0 for now, since we only
+% use the saved file for prediction making
 trainingParamsNode = docNode.createElement('traning_params');
 mlrtreesNode.appendChild(trainingParamsNode);
 
@@ -83,35 +80,51 @@ globalvaridxNode.appendChild(docNode.createTextNode('0'));
 mlrtreesNode.appendChild(globalvaridxNode);
 
 varidxNode = docNode.createElement('var_idx');
-varidxNode.appendChild(docNode.createTextNode('0 1'));
+varidxNode.appendChild(docNode.createTextNode(num2str(1:matrtree.var_count)));
 mlrtreesNode.appendChild(varidxNode);
 
 vartypeNode = docNode.createElement('var_type');
-vartypeNode.appendChild(docNode.createTextNode('0 0 1'));
+vartypeNode.appendChild(docNode.createTextNode(num2str(zeros(1,matrtree.var_count))));
 mlrtreesNode.appendChild(vartypeNode);
 
 catofsNode = docNode.createElement('cat_ofs');
-catofsNode.appendChild(docNode.createTextNode('0 0 0 0'));
+catofsNode.appendChild(docNode.createTextNode('0 0 0 0')); % Not sure what it is for now
 mlrtreesNode.appendChild(catofsNode);
 
-classlabelsNode = docNode.createElement('class_labels');
-classlabelsNode.appendChild(docNode.createTextNode('1 2'));
-mlrtreesNode.appendChild(classlabelsNode);
+% This might not be used for regresssion trees
+%classlabelsNode = docNode.createElement('class_labels');
+%classlabelsNode.appendChild(docNode.createTextNode('1 2')); 
+%mlrtreesNode.appendChild(classlabelsNode);
 
 missingsubstNode = docNode.createElement('missing_subst');
-missingsubstNode.appendChild(docNode.createTextNode('0. 0. 0.'));
+missingsubstNode.appendChild(docNode.createTextNode(num2str(zeros(1,matrtree.var_count))));
 mlrtreesNode.appendChild(missingsubstNode);
 
-ooberrorNode = docNode.createElement('oob_error');
-ooberrorNode.appendChild(docNode.createTextNode('0.'));
-mlrtreesNode.appendChild(ooberrorNode);
+% Out-of-bag permutations were not saved. Run with 'oobvarimp' set to 'on'.
+% in matlab 
+% ooberrorNode = docNode.createElement('oob_error'); 
+% ooberrorNode.appendChild(docNode.createTextNode('0.')); % Not saved
+% mlrtreesNode.appendChild(ooberrorNode);
 
 ntreesNode = docNode.createElement('ntrees');
-ntreesNode.appendChild(docNode.createTextNode('1'));
+ntreesNode.appendChild(docNode.createTextNode(num2str(matrtree.ntrees))); % Done
 mlrtreesNode.appendChild(ntreesNode);
 
+treesNode = docNode.createElement('trees');
 % --- Start the tree
-% TODO:
+for i = 1 : matrtree.trees
+    treeNode = docNode.createElement('_');
+    nodesNode = docNode.createElement('nodes');
+    for j = 1 : NumNodes
+        % Things get a bit complicated here, matlab saves the tree in a
+        % different manner regarding the openCV
+        % Matlab saves the topology by stating the node IDs in an array
+        % The implementation of writeTree in OpenCV - https://github.com/Itseez/opencv/blob/d7cb99254eb4e545bfe9c2d6bed1a2adbe30a547/modules/ml/src/tree.cpp#L1649
+        
+    end
+    treeNode.appendChild(nodesNode);
+    treesNode.appendChild(treeNode);
+end
 
 % --- Extract the path without extension and make new name for the xml
 [pathstr, name, ~] = fileparts(matrtreepath);
