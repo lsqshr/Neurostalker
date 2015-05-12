07f64ec949bca72cd9d38c51647d124813581fec
siqi liu <lsqshr@gmail.com>
2015-05-12 12:22:21 +1000

    remove the redundent norm in cart2sph

--- stalker_matlab/utils/cart2sph_sq.m
+++ stalker_matlab/utils/cart2sph_sq.m
@@ -4,10 +4,9 @@ function [th, phi, r] = cart2sph_sq(x, y, z)
 %                                2*pi > th > 0
 %                                pi > phi > 0
 
-%r = norm([x, y, z]);
 r = sqrt(x.^2 + y.^2 + z.^2);
 th = atan(y ./ x);
 phi = acos(z ./ r);
-th(th<0) = 2*pi + th(th<0);
+th(th<0) = 2 * pi + th(th<0);
 
 end
