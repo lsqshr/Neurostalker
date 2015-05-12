54f6750792a10b10636787c91079833124873521
siqi liu <lsqshr@gmail.com>
2015-05-12 14:53:50 +1000

    separate parameters to loadPara

--- stalker_matlab/scripts/train.m
+++ stalker_matlab/scripts/train.m
@@ -3,36 +3,41 @@ function train()
 % This version disregard the memory cost of loading training cases
 
 clc; clear all; close all;
-
-%------ PARA
-% TEST
-TESTSIZE        = 1;
-TESTSTARTLOC    = [[89, 363, 30]];
-TESTSTARTDIR    = [[-0.9278, 0.1916]];
-NTESTSTEP       = 100;
-STEPSIZE        = 1;
-NOISETYPE       = 'original'; % The noise added to vision blocks: original, gauss, salt_pepper
-% General Learning
-DEBUG           = true; % DEBUG=true will only train models with 1000 cases
-FRAMEWORK       = 'NORMAL' % The framework used for training/walking the neurostalker: 'NORMAL'/PUFFER
-PREFIX          = 'OP_';
-CACHETRAINDATA  = false;
-CACHETRAINMODEL = false;
-RUNTEST         = false;
-VBSIZE          = 13; % Predefined size of the vision box
-% Random Forest 
-RF.NTREE        = 200;
-RF.MTRY         = 140;
-% Neural Network
-NN.NHLAYER      = 2;
-NN.NHNEURON     = 13 * 5;
-NN.BATCHSIZE    = 1000;
-NN.SAEEPOCH     = 50;
-NN.NNEPOCH      = 100;
-NN.ZEROMASK     = 0.5;
-% ENDPARA
-
 curdir = fileparts(mfilename('fullpath'));
+
+if exist(fullfile(curdir, 'loadPara.m'))
+    disp('Loading parameters from loadPara.m')
+    loadPara
+else
+    %------ PARA
+    % TEST
+    TESTSIZE        = 1;
+    TESTSTARTLOC    = [[89, 363, 30]];
+    TESTSTARTDIR    = [[-0.9278, 0.1916]];
+    NTESTSTEP       = 100;
+    STEPSIZE        = 1;
+    NOISETYPE       = 'original'; % The noise added to vision blocks: original, gauss, salt_pepper
+    % General Learning
+    DEBUG           = true; % DEBUG=true will only train models with 1000 cases
+    FRAMEWORK       = 'NORMAL' % The framework used for training/walking the neurostalker: 'NORMAL'/PUFFER
+    PREFIX          = 'OP_';
+    CACHETRAINDATA  = false;
+    CACHETRAINMODEL = false;
+    RUNTEST         = false;
+    VBSIZE          = 13; % Predefined size of the vision box
+    % Random Forest 
+    RF.NTREE        = 200;
+    RF.MTRY         = 140;
+    % Neural Network
+    NN.NHLAYER      = 2;
+    NN.NHNEURON     = 13 * 5;
+    NN.BATCHSIZE    = 1000;
+    NN.SAEEPOCH     = 50;
+    NN.NNEPOCH      = 100;
+    NN.ZEROMASK     = 0.5;
+    % ENDPARA
+end
+
 datadir = fullfile(curdir, '..', 'data', 'input', 'preprocessed');
 addpath(genpath(fullfile(curdir, '..', 'utils')));
 addpath(genpath(fullfile(curdir, '..', 'lib', 'DeepLearnToolbox')));
