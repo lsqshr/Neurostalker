72a1d00f5893ea30e168b0db153d67e355ff8041
siqi liu <lsqshr@gmail.com>
2015-05-13 11:17:08 +1000

    add back pbs to move cache model to data folder on hpc

--- stalker_matlab/scripts/train.m
+++ stalker_matlab/scripts/train.m
@@ -19,7 +19,7 @@ else
     NOISETYPE       = 'original'; % The noise added to vision blocks: original, gauss, salt_pepper
     % General Learning
     DEBUG           = true; % DEBUG=true will only train models with 1000 cases
-    FRAMEWORK       = 'NORMAL' % The framework used for training/walking the neurostalker: 'NORMAL'/PUFFER
+    FRAMEWORK       = 'NORMAL'; % The framework used for training/walking the neurostalker: 'NORMAL'/PUFFER
     PREFIX          = 'OP_';
     CACHETRAINDATA  = false;
     CACHETRAINMODEL = false;
