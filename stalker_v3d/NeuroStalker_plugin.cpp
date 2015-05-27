55c35618e3cc4ce0629e461a1a6e1d7ac579e8c2
donghaozhang <dzha9516@uni.sydney.edu.au>
2015-05-27 16:29:00 +1000

    crop image

--- stalker_v3d/NeuroStalker_plugin.cpp
+++ stalker_v3d/NeuroStalker_plugin.cpp
@@ -12,6 +12,8 @@
 
 #include "NeuroStalker_plugin.h"
 #include "lib/ImageOperation.h"
+#include "stackutil.h"
+//#include "../../v3d_main/basic_c_fun/basic_memory.cpp"//note: should not include .h file, since they are template functions
 
 ImageOperation *IM;
 
@@ -152,6 +154,7 @@ void reconstruction_func(V3DPluginCallback2 &callback, QWidget *parent, input_PA
 
 
         PARA.inimg_file = p4DImage->getFileName();
+        if(p4DImage) {delete p4DImage;};
     }
     else
     {
@@ -173,9 +176,64 @@ void reconstruction_func(V3DPluginCallback2 &callback, QWidget *parent, input_PA
         c = PARA.channel;
     }
 
-
     // Main neuron reconstruction code
+//-----------------------------------------------------------------------------------------
+    printf("1. Find the bounding box and crop image. \n");
+    long l_boundbox_min[3], l_boundbox_max[3];//xyz
+    V3DLONG sz_img_crop[4];
+    long l_npixels_crop;
+    unsigned char *p_img8u_crop = 0;
+    
+    //find bounding box
+    unsigned char ***p_img8u_3d = 0;
+    if(!new3dpointer(p_img8u_3d ,in_sz[0], in_sz[1], in_sz[2], data1d))
+    {
+        printf("ERROR: Fail to allocate memory for the 4d pointer of image.\n");
+        if(p_img8u_3d) {delete3dpointer(p_img8u_3d, in_sz[0], in_sz[1], in_sz[2]);}
+    }
+    printf("boundingbox x dimension: %d,y dimension: %d,z dimension: %d.\n", in_sz[0], in_sz[1], in_sz[2]);
+    l_boundbox_min[0] = in_sz[0];  l_boundbox_min[1] = in_sz[1];  l_boundbox_min[2] = in_sz[2];
+    l_boundbox_max[0] = 0;                l_boundbox_max[1] = 0;                l_boundbox_max[2] = 0;
+    for(long X=0;X<in_sz[0];X++)
+        for(long Y=0;Y<in_sz[1];Y++)
+            for(long Z=0;Z<in_sz[2];Z++)
+                if(p_img8u_3d[Z][Y][X]>0.1)
+                {
+                    if(l_boundbox_min[0] > X) l_boundbox_min[0] = X;    if(l_boundbox_max[0] < X) l_boundbox_max[0] = X;
+                    if(l_boundbox_min[1] > Y) l_boundbox_min[1] = Y;    if(l_boundbox_max[1] < Y) l_boundbox_max[1] = Y;
+                    if(l_boundbox_min[2] > Z) l_boundbox_min[2] = Z;    if(l_boundbox_max[2] < Z) l_boundbox_max[2] = Z;
+                }
+    printf(">>boundingbox: x[%ld~%ld],y[%ld~%ld],z[%ld~%ld]\n",l_boundbox_min[0], l_boundbox_max[0],
+                                                               l_boundbox_min[1], l_boundbox_max[1],
+                                                               l_boundbox_min[2], l_boundbox_max[2]);
 
+    //crop image
+    sz_img_crop[0] = l_boundbox_max[0] - l_boundbox_min[0] + 1;
+    sz_img_crop[1] = l_boundbox_max[1] - l_boundbox_min[1] + 1;
+    sz_img_crop[2] = l_boundbox_max[2] - l_boundbox_min[2] + 1;
+    sz_img_crop[3] = 1;
+    l_npixels_crop = sz_img_crop[0] * sz_img_crop[1] * sz_img_crop[2];
+
+    p_img8u_crop = new(std::nothrow) unsigned char[l_npixels_crop]();
+    if(!p_img8u_crop)
+    {
+        printf("ERROR: Fail to allocate memory for p_img32f_crop!\n");
+        if(p_img8u_3d)              {delete3dpointer(p_img8u_3d, in_sz[0], in_sz[1], in_sz[2]);}
+    }
+    unsigned char *p_tmp = p_img8u_crop;
+    for(long Z = 0;Z < sz_img_crop[2];Z++)
+        for(long Y = 0;Y < sz_img_crop[1];Y++)
+            for(long X = 0;X < sz_img_crop[0];X++)
+            {
+                *p_tmp = p_img8u_3d[Z+l_boundbox_min[2]][Y+l_boundbox_min[1]][X+l_boundbox_min[0]];
+                p_tmp++;
+            }
+    if(p_img8u_3d) {delete3dpointer(p_img8u_3d, in_sz[0], in_sz[1], in_sz[2]);}
+    saveImage("test/testdata/crop.v3draw", p_img8u_crop, sz_img_crop, V3D_UINT8);
+    printf("success\n");
+//-----------------------------------------------------------------------------------------
+
+    data1d = p_img8u_crop;
     // Using the Image Operation found in vaa3d_tools/hackathon/zhi/snake_tracing/TracingCore/ in for some simple Image Processing
     IM = new ImageOperation;
 
@@ -184,7 +242,7 @@ void reconstruction_func(V3DPluginCallback2 &callback, QWidget *parent, input_PA
 
     for(int i = 0; i < 4; i++)
     {
-        in_sz_int[i] = (int)in_sz[i];
+        in_sz_int[i] = (int)sz_img_crop[i];
     }
 
     cout<<"in_sz_int:\t"<<endl; 
@@ -195,6 +253,7 @@ void reconstruction_func(V3DPluginCallback2 &callback, QWidget *parent, input_PA
     std::copy(in_sz,
        in_sz + sizeof(in_sz) / sizeof(in_sz[0]),
        ostream_iterator<V3DLONG>(cout, "\n")); 
+    
     IM->Imcreate(data1d, in_sz_int);
     // Preprocessing
     std::cout<<"=== Compute Gradient Vector Flow..."<<std::endl;
@@ -210,7 +269,7 @@ void reconstruction_func(V3DPluginCallback2 &callback, QWidget *parent, input_PA
     // Adaptive Tracing here, may replace with graph cut
     IM->ImComputeInitBackgroundModel(IM->v_threshold);
     IM->ImComputeInitForegroundModel();
-
+    
     //Output
     NeuronTree nt;
 	QString swc_name = PARA.inimg_file + "_NeuroStalker.swc";
@@ -219,10 +278,11 @@ void reconstruction_func(V3DPluginCallback2 &callback, QWidget *parent, input_PA
 
     if(!bmenu)
     {
-        if(data1d) {delete []data1d; data1d = 0;}
+        if(data1d) {delete [] data1d; data1d = 0;}
+        //if(p_img8u_crop) {delete [] p_img8u_crop;  p_img8u_crop = 0;}
     }
 
     v3d_msg(QString("Now you can drag and drop the generated swc fle [%1] into Vaa3D.").arg(swc_name.toStdString().c_str()),bmenu);
-
+            
     return;
 }
