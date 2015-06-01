688864ccc3c15bf9ff3b0b1d0207c71ddf0e7a4c
siqi liu <lsqshr@gmail.com>
2015-06-01 19:20:44 +1000

    change interface of PressureSampler. position of a voxel is defined as int

--- stalker_v3d/PressureSampler.h
+++ stalker_v3d/PressureSampler.h
@@ -13,17 +13,18 @@ public:
     	             float radius = 0);
     ~PressureSampler();
 	void RandRotateSph();
-	void RandSample(int x, int y, int z);
+	void RandSample(double x, double y, double z); // Randomly sample the pressure at specific postion (x,y,z)
 	float *  GetBaseTh();
 	float *  GetBasePhi();
 	float *  GetPeakTh();
 	float *  GetPeakPhi();
+	friend void test(); // For Unit-Testing
 
 private:
 	unsigned char * OriginalImg;
 	unsigned char * BinaryImg;
 	float * GVF;
-    float radius;	
+    float radius;
 	int ndir; // Number of directions
     float density; // The density of the sampled points on each sample plane
 	float * baseth; // The theta values of the base vectors
@@ -31,7 +32,7 @@ private:
     float * lpressure; // The pressure sampled at each direction
     float * SampleVoxels(int * lx, int * ly, int * lz, int sz);
 	void GenSph(); // Generate the base spherical directions
-	void FindVoxel2Sample(float x, float y, float z, float th, float phi);
+	void FindVoxel2Sample(int x, int y, int z, double th, double phi);
 };
 
 #endif
