eaca9773581ce66f7dd516499b8190d8dc0e015b
siqi liu <lsqshr@gmail.com>
2015-06-01 12:23:33 +1000

    Change GenSph to modify the member bases

--- stalker_v3d/PressureSampler.cpp
+++ stalker_v3d/PressureSampler.cpp
@@ -47,20 +47,18 @@ void PressureSampler::GenSph(){
     vectype v; 
     v = linspace(0, 1, nphi);
 
-    double * repu = new double[u.size() * nphi];
+    double * repu = new double[this->ndir];
     repmat1d(u, nphi, 2, repu);
 
-    double * repv = new double[v.size() * nth];
+    double * repv = new double[this->ndir];
     repmat1d(v, nth, 2, repv);
-    double * transv = new double[v.size() * nth];
+    double * transv = new double[this->ndir];
     transpose(repv, transv, v.size(), nth);// Transpose repv matrix
 
-    double * th = new double[u.size() * nphi];
     for (int i = 0; i < u.size() * nphi; i++)
-        th[i] = 2 * M_PI * repu[i];
+        this->baseth[i] = 2 * M_PI * repu[i];
 
-    double * phi = new double[v.size() * nth];
     for (int i = 0; i < v.size() * nth; i++)
-        phi[i] = acos(2 * transv[i] - 1);
+        this->basephi[i] = acos(2 * transv[i] - 1);
 
 }
