b254c394de734304d2dcbb1bd19e86701e597c32
donghaozhang <dzha9516@uni.sydney.edu.au>
2015-07-22 12:20:42 +1000

    The step size and step are now two input parameters, but threshold value is still a problem.

--- stalker_v3d/teststalker.sh
+++ stalker_v3d/teststalker.sh
@@ -6,6 +6,6 @@ qmake;
 make -j4;
 
 #$vaa3d -x NeuroStalker -f tracing_func -i test/testdata/test.little-soma.v3draw -p 1 3 3 | tee runlog.txt
-$vaa3d -x NeuroStalker -f tracing_func -i test/test.v3draw -p 1 1 1 | tee runlog.txt
+$vaa3d -x NeuroStalker -f tracing_func -i test/09-2902-04R-01C-60x_merge_c1.v3dpbd -p 1 1 1 5 5 | tee runlog.txt
 #$vaa3d -x NeuroStalker -f tracing_func -i ~/Desktop/OP1.v3draw -p 1 3 3 | tee runlog.txt
 #matlab -nodesktop -nosplash -r "run('test/plotall.m')";
